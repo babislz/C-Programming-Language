75a182978949e2d704843386e62258ad048058d1
coooos <133787898+coooos@users.noreply.github.com>
2023-09-27 11:15:06 -0300

    Add files via upload

---
+++ retom.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+// void somar(int *x, int *y, int *soma)
+// {
+//     *soma = *y + *x;
+// }
+
+// int main()
+// {
+//     int soma, x, y;
+
+//     printf("Insira o primeiro numero: ");
+//     scanf("%d", &x);
+
+//     printf("Insira o segundo numero: ");
+//     scanf("%d", &y);
+
+//     somar(&x, &y, &soma);
+
+//     printf("%d", soma);
+// }
+
+//=================================================//
+
+// int main(){
+//     int arr_valores[5] = {22,33,74,15,2};
+//     int *ptr = arr_valores;
+
+//     printf("%d", *(ptr + 4));
+// }
+
+//=================================================//
+
+// void arrayy(){
+//     int array_valores[4] = {1, 55, 27, 22};
+//     int *ptr = array_valores;
+
+//     for (int i = 0; i < 4; i++)
+//     {
+//         printf("%d ", *(ptr + i));
+//     }
+    
+// }
+// int main(){
+//     arrayy();
+// }
+
+//=================================================//
+
+// void inverter(){
+//     char palavra[7][1] = {"t","e","c","l","a","d","o"};
+//     char *ptr = palavra;
+
+//     for (char i = 0; i < 7; i++)
+//     {
+//         printf("%c", *(ptr + 6 - i));
+//     }
+    
+// }
+// int main(){
+//     inverter();
+// }
+
+//=================================================//
+
+void inverter(){
+    char palavra[7][1] = {"t","e","c","l","a","d","o"};
+    char *ptr = palavra;
+
+    for (char i = 0; i < 7; i++)
+    {
+        
+    }
+    
+}
+int main(){
+    inverter();
+}
