41c338327db264beb1ec0ead9bd5ff60bb587a72
coooos <gabriela.laureano@escola.pr.gov.br>
2023-09-29 11:00:54 -0300

    novo vidade

--- matriz.c
+++ matriz.c
@@ -1,4 +1,49 @@
 #include <stdio.h>
-    void exibir(){
+    
+    int main(void){
+    
+    int linha;
+    int coluna;
+    int maior = 0;
+    int maior_linha;
+    int maior_coluna;
+
+
+    printf("Linhas: ");
+    scanf("%d", &linha);
+    printf("Colunas: ");
+    scanf("%d", &coluna);
+
+    int mato[linha][coluna];
+
+    for (int i = 0; i < linha; i++)
+    {
+        for (int j = 0; j < coluna; j++)
+        {
+            scanf(" %d", &mato[i][j]);
+            if (mato[i][j] > maior)
+            {
+                maior = mato[i][j];
+                maior_linha = i;
+                maior_coluna = j;
+            }
             
+        }
     }
+
+    printf("Matrix lida:\n");
+    for (int i = 0; i < linha; i++)
+    {
+        for (int j = 0; j < coluna; j++)
+        {
+            printf(" %d", mato[i][j]);
+        }
+
+        printf("\n");
+    }
+    printf("Maior Numero: %d\n", maior);
+    printf("Linha: %d\n", maior_linha);
+    printf("Coluna: %d\n", maior_coluna);
+}
+            
+    
